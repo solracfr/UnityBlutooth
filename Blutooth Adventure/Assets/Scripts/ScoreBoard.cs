4df034069b6bc74d9e14d6480ec4238782df05a0
solracfresno <solracfresno@gmail.com>
2021-09-26 22:06:55 -0700

    canvas and score canvas implementation

--- Blutooth Adventure/Assets/Scripts/ScoreBoard.cs
+++ Blutooth Adventure/Assets/Scripts/ScoreBoard.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreBoard : MonoBehaviour
 {
     int score; // initiates at 0
-    
+    TMP_Text scoreText;
+
+    private void Start() 
+    {
+        scoreText = GetComponent<TMP_Text>();
+        scoreText.text = "Start";
+    }
 
     public void IncreaseScore(int amtToIncrease)
     {
         score += amtToIncrease;
+        scoreText.text = score.ToString();
         Debug.Log($"Score is now {score}.");
     }
 }
